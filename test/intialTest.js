a4c67793cdff12c10e3198f46c6137d880ccedf1
Niranjan Kurra <kurraniranjan31@gmail.com>
2018-02-20 12:20:51 -0600

    adding basic tests

---
+++ test/intialTest.js
@@ -0,0 +1,8 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('basic test', function () {
+  it('should add 1 plus 1 to be two', function () {
+    expect(1+1).to.equal(2);
+  });
+});
